10030c6fa1573692156c9e28ede0992a29037b44
769344359 <201308030321@hnu.edu.cn>
2017-10-31 18:39:48 +0800

    匿名内存
    注释

--- sapi/fpm/fpm/fpm_shm.c
+++ sapi/fpm/fpm/fpm_shm.c
@@ -21,7 +21,7 @@ void *fpm_shm_alloc(size_t size) /* {{{ */
 {
 	void *mem;
 
-	mem = mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+	mem = mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);    //核心代码mmap 分配共享内存
 
 #ifdef MAP_FAILED
 	if (mem == MAP_FAILED) {
@@ -35,12 +35,12 @@ void *fpm_shm_alloc(size_t size) /* {{{ */
 		return NULL;
 	}
 
-	fpm_shm_size += size;
+	fpm_shm_size += size;          // 内部全部变量添加
 	return mem;
 }
 /* }}} */
 
-int fpm_shm_free(void *mem, size_t size) /* {{{ */
+int fpm_shm_free(void *mem, size_t size) /* {{{ */      // 释放匿名内存
 {
 	if (!mem) {
 		zlog(ZLOG_ERROR, "mem is NULL");
@@ -62,7 +62,7 @@ int fpm_shm_free(void *mem, size_t size) /* {{{ */
 }
 /* }}} */
 
-size_t fpm_shm_get_size_allocated() /* {{{*/
+size_t fpm_shm_get_size_allocated() /* {{{*/           //获得分配的总的匿名内存的大小
 {
 	return fpm_shm_size;
 }
