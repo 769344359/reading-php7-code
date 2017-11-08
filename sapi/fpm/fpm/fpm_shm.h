7a5ad21b42ceeb35c644fa2680d25154a2bbaea2
dinosaur <769344359@qq.com>
2017-11-09 00:05:56 +0800

    测试

--- sapi/fpm/fpm/fpm_shm.h
+++ sapi/fpm/fpm/fpm_shm.h
@@ -5,7 +5,7 @@
 #ifndef FPM_SHM_H
 #define FPM_SHM_H 1
 
-void *fpm_shm_alloc(size_t size);
+void *fpm_shm_alloc(size_t size);      // 只有三个简单的函数
 int fpm_shm_free(void *mem, size_t size);
 size_t fpm_shm_get_size_allocated();
 
